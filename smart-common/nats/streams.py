fa7ace7c93dd530804c48cd177ad66462fd42e91
Michał Pielak <michal.pielak81@gmail.com>
2025-12-13 13:15:31 +0100

    chore: remove smart-common from repo (prepare for submodule)

--- smart-common/nats/streams.py
+++
@@ -1,6 +0,0 @@
-DEVICE_COMM_STREAM = {
-    "name": "device_communication",
-    "subjects": ["device_communication.>"],
-    "retention": "limits",
-    "storage": "file"
-}
--- smart-common/nats/subjects.py
+++
@@ -1,3 +0,0 @@
-INVERTER_UPDATE = "device_communication.inverter.{serial}.production.update"
-RASPBERRY_HEARTBEAT = "device_communication.raspberry.{uuid}.heartbeat"
-RASPBERRY_EVENTS = "device_communication.raspberry.{uuid}.events"
